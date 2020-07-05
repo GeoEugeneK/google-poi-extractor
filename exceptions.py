987f2904382a0eea83d395f034c91bc59a754bab
GeoEugeneK <gis.kalinouski@gmail.com>
2020-07-06 02:56:16 +0300

    implemented traceback writing function

--- exceptions.py
+++ exceptions.py
@@ -1,3 +1,8 @@
+import traceback
+from typing import List
+import datetime
+
+
 class InvalidPoiDataError(Exception):
     """ Gets raised when PoiData instance cannot be created. """
 
@@ -28,3 +33,17 @@ class InvalidRequestException(Exception):
 
 class FinishException(Exception):
     pass
+
+
+def write_traceback(e: Exception, file: str, append: bool = True):
+
+    """ Writes traceback to a file in specified mode."""
+
+    calls: List[str] = traceback.format_tb(e.__traceback__)
+    formatted_tb = "".join(calls)
+
+    timemark = datetime.datetime.now().strftime("%b %d, %H-%M-%S")
+
+    mode = "a" if append else "w"
+    with open(file, mode, encoding="utf-8-sig") as f:
+        f.write(f"\n\n\n{timemark}:   TRACEBACK\n" + formatted_tb + str(e))
